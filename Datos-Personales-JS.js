203e93a7f6800bc44f4ec063f19dc6281f8cd5da
RoCo3108 <roco272012@hotmail.com>
2022-12-12 23:17:41 -0500

    Ejercicio-JS-Tipos-Datos

---
+++ Datos-Personales-JS.js
@@ -0,0 +1,9 @@
+const datos = ["Ronald Cortes", 38, true, "22/julio/1984", libro = {
+    nombre: "negocio del siglo XXI",
+    Autor: "Kiyosaki",
+    Fecha: "2014",
+    url: "https://economicas.unsa.edu.ar/afinan/informacion_general/novedades_bibliograficas/detalle/libro_213.htm"
+}]
+
+
+console.log(datos[1]," " + libro.url);
