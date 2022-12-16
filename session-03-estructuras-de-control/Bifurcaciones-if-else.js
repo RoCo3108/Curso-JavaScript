fb306bfbdd9c3aaf79acc72afe74dc205faf23d6
RoCo3108 <roco272012@hotmail.com>
2022-12-16 00:56:08 -0500

    if else

---
+++ session-03-estructuras-de-control/Bifurcaciones-if-else.js
@@ -0,0 +1,15 @@
+//Bifurcaciones if ....else
+let saldo = 50000;
+let efectivo = 10000;
+
+
+
+if (efectivo < saldo) {
+    console.log("es verdadero");
+}
+
+if (efectivo < saldo) {
+    console.log("Es verdadero");
+} else {
+    console.log("Saldo Insuficiente");
+}
